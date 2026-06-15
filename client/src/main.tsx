4028b1ddf58f295cc828d43d0ade44d1fca0d164
gauthierntudi <gauthierntudi@gmail.com>
2026-06-15 19:43:49 +0100

    ajuste UI

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import App from './App.tsx';
 
-if ('serviceWorker' in navigator) {
+if (import.meta.env.PROD && 'serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/sw.js').catch(() => undefined);
   });
